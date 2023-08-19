826586d7e02eb238df637d40c38b39e2452e08be
Tan Jim Son <dahfreeman@hotmail.com>
2023-08-20 01:22:44 +0800

    Add files via upload

---
+++ src/components/layout/NPCView.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="person-list-container">
+		<Person v-for="item in this.npcs" :key="item.name" :person="item" />
+	</div>
+</template>
+
+<script>
+import Person from '@/components/Person.vue';
+
+export default {
+	components: {
+		Person
+	},
+	props: {
+		npcs: {
+			type: Array,
+			required: true,
+		}
+	},
+}
+</script>

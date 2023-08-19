88a690ab648b95d38754590c30336078e318d56a
Tan Jim Son <dahfreeman@hotmail.com>
2023-08-20 02:01:15 +0800

    Add files via upload

---
+++ src/components/layout/PilotsView.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="pilot-list-container">
+		<Pilot v-for="item in this.pilots" :key="item.callsign" :pilot="item" />
+	</div>
+</template>
+
+<script>
+import Pilot from '@/components/Pilot.vue';
+
+export default {
+	components: {
+		Pilot
+	},
+	props: {
+		pilots: {
+			type: Object,
+			required: true,
+		}
+	},
+}
+</script>

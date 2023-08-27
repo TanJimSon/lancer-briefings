94b17c9a8bf599ca46a94943ab6bf625abba1aff
Tan Jim Son <dahfreeman@hotmail.com>
2023-08-28 01:30:38 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,7 @@ export default {
         {
           "slug": "001",
           "name": "Bug-Hunt",
-          "status": "start"
+          "status": "success"
         },
       ],
       "pilots": [
